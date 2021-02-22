6ba536464120fc4b2e626a423b2c6c606f76b8d6
Jrc <jrc0117@163.com>
2021-02-22 12:03:49 +0800

    fix title

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-const app = createApp(App)
 
-// 分环境处理
-if (process.env.NODE_ENV === 'development') {
-  if ('__VUE_DEVTOOLS_GLOBAL_HOOK__' in window) {
-    // 这里__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue赋值一个createApp实例
-    window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app
-  }
-  app.config.devtools = true
-}
-
-app.mount('#app')
+createApp(App).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   publicPath: '/games/puzzle/',
-  title: '成语字谜小游戏',
+  // eslint-disable-next-line no-unused-vars
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = '成语字谜 Vue3';
+        return args;
+      })
+  }
 }
